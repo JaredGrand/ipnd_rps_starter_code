288f8deb4530e389670a5b8637008df31ffda46c
Jared Grandlienard <jared.a.grandlienard@gmail.com>
2018-10-31 08:44:36 -0500

    Imported the random module

--- starter_code.py
+++ starter_code.py
@@ -3,6 +3,8 @@
 """This program plays a game of Rock, Paper, Scissors between two Players,
 and reports both Player's scores each round."""
 
+import random
+
 moves = ['rock', 'paper', 'scissors']
 
 """The Player class is the parent class for all of the Players
